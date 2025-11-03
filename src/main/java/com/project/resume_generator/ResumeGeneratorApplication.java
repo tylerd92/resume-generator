ac0928c4c3d96c0b6fd686f3044021d6375f644c
tylerd92 <twdeans@gmail.com>
2025-11-03 01:42:34 -0500

    set up maven project

---
+++ src/main/java/com/project/resume_generator/ResumeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.project.resume_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/resume_generator/ResumeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.resume_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
