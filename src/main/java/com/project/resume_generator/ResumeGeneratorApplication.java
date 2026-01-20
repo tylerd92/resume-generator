1d5fcd8b09eda06694ddc9609798550a1096e3e7
tylerd92 <twdeans@gmail.com>
2026-01-20 02:07:16 -0500

    Update ResumeGeneratorApplication class

--- src/main/java/com/project/resume_generator/ResumeGeneratorApplication.java
+++ src/main/java/com/project/resume_generator/ResumeGeneratorApplication.java
@@ -1,10 +1,30 @@
 package com.project.resume_generator;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import picocli.CommandLine;
 
 @SpringBootApplication
-public class ResumeGeneratorApplication {
+public class ResumeGeneratorApplication implements CommandLineRunner, ExitCodeGenerator {
+
+    private final ResumeCommand resumeCommand;
+    private int exitCode;
+
+    public ResumeGeneratorApplication(ResumeCommand resumeCommand) {
+        this.resumeCommand = resumeCommand;
+    }
+
+    @Override
+    public void run(String... args) {
+        exitCode = new CommandLine(resumeCommand).execute(args);
+    }
+
+    @Override
+    public int getExitCode() {
+        return exitCode;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResumeGeneratorApplication.class, args);
